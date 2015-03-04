d1813c30c6cb4163128ad05c3041a074aae9456d
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2015-03-04 14:43:56 +0100

    Merge instr.v

--- src/amd64/instr.v
+++ src/amd64/instr.v
@@ -13,19 +13,41 @@
 Require Import Ssreflect.ssreflect.
 Require Import bitsrep reg.
 
+Definition baseReg := gpReg.
+Definition ixReg := nonSPReg.
+Coercion gpReg_to_baseReg (r:gpReg) : baseReg := r.
+Coercion nonSPReg_to_baseReg (r:nonSPReg) : baseReg := r.
+
+
+
 Inductive scale := S1 | S2 | S4 | S8.
 
 Inductive memspec :=
-  mkMemSpec (sib : option reg * option (gpReg * scale))
-            (offset : option DWORD).
+  mkMemSpec (sib : option baseReg * option (ixReg * scale))
+            (offset : option DWORD)
+| RIPrel (displacement: DWORD).
+
+(* Immediates are maximum 32-bits, sign-extended to 64-bit if necessary *)
+Definition IMM s := 
+  BITS (match s with
+          | OpSize1 => n8
+          | OpSize2 => n16
+          | OpSize4 => n32
+          | OpSize8 => n32
+        end).
+
 
 (* Register or memory *)
 Inductive regmem s :=
 | RegMemR (r: gpVReg s) :> regmem s
 | RegMemM (ms: memspec).
 Inductive regimm s :=
-| RegImmI (c: VWORD s)
+| RegImmI (c: IMM s)
 | RegImmR (r: gpVReg s) :> regimm s.
+Inductive XMem :=
+| XMemR (r: XMMreg) :> XMem 
+| XMemM (ms: memspec).
+
 
 Coercion DWORDRegMemM (ms: memspec) := RegMemM OpSize4 ms.
 Coercion DWORDRegImmI (d: DWORD)    := RegImmI OpSize4 d.
@@ -41,19 +63,29 @@ Inductive dstsrc (s: opsize) :=
 | DstSrcRR (dst src: gpVReg s)
 | DstSrcRM (dst: gpVReg s) (src: memspec)
 | DstSrcMR (dst: memspec) (src: gpVReg s)
-| DstSrcRI (dst: gpVReg s) (c: VWORD s)
-| DstSrcMI (dst: memspec) (c: VWORD s).
+| DstSrcRI (dst: gpVReg s) (c: IMM s)
+| DstSrcMI (dst: memspec) (c: IMM s).
+
+(* TODO: careful with using dstsrc as movdstsrc: RI is restrictevely
+sized. *)
+
+Inductive imuldstsrc (s: opsize) :=
+| ImulDstSrcRR (dst: gpVReg s)(src: gpVReg s)
+| ImulDstSrcRM (dst: gpVReg s)(src: memspec)
+| ImulDstSrcRRI (dst: gpVReg s)(src: gpVReg s)(c: IMM s)
+| ImulDstSrcRMI (dst: gpVReg s)(src: memspec)(c: IMM s).
+
 
 (* We could support 32-bit operands there (for MOVD) *)
 Inductive xmmdstsrc :=
-| XMMDstSrcXRM (dst : XMMreg) (src : regmem OpSize8)
-| XMMDstSrcRMX (dst : regmem OpSize8) (src : XMMreg).
+| XMMDstSrcXRM (dst : XMMreg) (src : XMem)
+| XMMDstSrcRMX (dst : XMem) (src : XMMreg).
 
 (* Jump target: PC-relative offset *)
 (* We make this a separate type constructor to pick up type class instances later *)
 (* Jump ops: immediate, register, or memory source *)
 Inductive Tgt :=
-| mkTgt :> DWORD -> Tgt.
+| mkTgt :> QWORD -> Tgt.
 Inductive JmpTgt :=
 | JmpTgtI :> Tgt -> JmpTgt
 | JmpTgtM :> memspec -> JmpTgt
@@ -81,18 +113,17 @@ Inductive Condition :=
 Inductive Instr :=
 | UOP s (op: UnaryOp) (dst: regmem s)
 | BOP s (op: BinOp) (ds: dstsrc s)
-| BITOP (op: BitOp) (dst: regmem OpSize4) (bit: gpReg + BYTE)
+| BITOP (op: BitOp) (dst: regmem OpSize4) (bit: gpReg32 + BYTE)
 | TESTOP s (dst: regmem s) (src: regimm s)
 | MOVOP s (ds: dstsrc s)
 | MOVABS (dst : gpReg) (src : QWORD)
-| MOVX (signextend:bool) s (dst: gpReg) (src: regmem s)
+| MOVX (signextend:bool) (dst: gpReg32) (src: regmem OpSize1)
 | MOVQ (ds : xmmdstsrc)
 | SHIFTOP s (op: ShiftOp) (dst: regmem s) (count: ShiftCount)
 | MUL {s} (src: regmem s)
-| IMUL s (dst: gpVReg s) (src: regmem s)
-| IMULimm s (dst: gpVReg s) (src: regmem s) (i : VWORD s)
+| IMUL s (dst: imuldstsrc s)
 | IDIV {s} (src : regmem s)
-| LEA (reg: gpReg) (src: regmem OpSize4)
+| LEA s (reg: gpVReg s) (src: regmem s)
 | XCHG s (reg: gpVReg s) (src: regmem s)
 | JCCrel (cc: Condition) (cv: bool) (tgt: Tgt)
 | SET (cc : Condition) (cv : bool) (r : gpVReg OpSize1)
@@ -100,8 +131,8 @@ Inductive Instr :=
 (* Only 64-bit values can be pushed, although we could have allowed 16-bit
 values too. Push for 32-bit values cannot be encoded in x86-64. *)
 | PUSH (src: src)
-| POP (dst: regmem OpSize4)
+| POP s (dst: regmem s)
 | CALLrel (tgt: JmpTgt) | JMPrel (tgt: JmpTgt)
 | RETOP (size: WORD)
 | CQO
-| HLT | ENCLU | ENCLS | BADINSTR.
+| HLT | BADINSTR.
